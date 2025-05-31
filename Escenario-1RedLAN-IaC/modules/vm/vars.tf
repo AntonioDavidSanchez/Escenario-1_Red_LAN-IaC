b061c4f2e4011d9c6c5f2a60fd73c342d4185c81
AntonioDavidSanchez <antoniodavidsanchezmolina@gmail.com>
2025-05-31 18:39:41 +0200

    Add files via upload

---
+++ Escenario-1RedLAN-IaC/modules/vm/vars.tf
@@ -0,0 +1,20 @@
+# modules/vm/vars.tf
+variable "vm_name" {}
+
+variable "vm_description" {}
+
+variable "node_name" {}
+
+variable "vm_id" {}
+
+variable "vm_tags" {}
+
+variable "dns_servers" {}
+
+variable "ipv4_address" {}
+
+variable "ipv4_gateway" {}
+
+variable "clone_vm_id" {}
+
+variable "clone_full" {}
